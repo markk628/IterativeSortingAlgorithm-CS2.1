7b21720be2823c39f4c174de23bd895c546e449c
Donghyun (Mark) Kim <markk628@gmail.com>
2020-10-30 23:38:12 -0700

    recursive added

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,27 +1,35 @@
 import time
 
-def is_sorted(items):
-    for i in range(len(items) - 1):
-        if items[i] >= items[i + 1]:
-            return False
-    return True
-
 def insertion_sort(items):
-    # for i in range(len(items) - 1):
-    #     for j in range(i + 1, len(items)):
-    #         if items[i] > items[j]:
-    #             items[i], items[j] = items[j], items[i]
-    # return items
-    while is_sorted(items) == False:
-        first_index = 0
-        second_index = 1
-        if items[first_index] > items[second_index]:
-            items[first_index], items[second_index] = items[second_index], items[first_index]
-        if second_index < len(items) - 1:
-            first_index += 1   
-            second_index += 1
-    return items   
+    for i in range(1, len(items)):
+        second_value = items[i]
+        second_index = i
+        while second_index > 0 and items[second_index - 1] > second_value:
+            items[second_index] = items[second_index - 1]
+            second_index -= 1
+        items[second_index] = second_value
+    return items
 
 start_time = time.time()
 print(insertion_sort([9,4,17,2]))
+print("%.10f seconds" % (time.time() - start_time))
+
+def insertion_sort_recursive(items, n):
+    if n <= 1:
+        return
+
+    insertion_sort_recursive(items,n - 1)
+
+    second_value = items[n - 1]
+    first_index = n-2
+    while (first_index >= 0 and items[first_index] > second_value):
+        items[first_index + 1] = items[first_index]
+        first_index = first_index - 1
+    items[first_index + 1]=second_value
+    return items
+
+start_time = time.time()
+arr = [9,4,17,2]
+n = len(arr)
+print(insertion_sort_recursive(arr, n))
 print("%.10f seconds" % (time.time() - start_time))
